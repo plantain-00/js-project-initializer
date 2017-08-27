b3683223bdfb5ec8de861a2a6b553e98a05d6ff0
york yao <yaoao12306@outlook.com>
2017-08-27 09:41:08 +0800

    add and save screenshot source

--- src/backend-with-frontend.ts
+++ src/backend-with-frontend.ts
@@ -94,7 +94,8 @@ module.exports = {
       'rev-static --config static/rev-static.config.js',
       async () => {
         const puppeteer = require('puppeteer')
-        const server = childProcess.exec('node ./dist/index.js')
+        const fs = require('fs')
+        const server = childProcess.spawn('node', ['./dist/index.js'])
         server.stdout.pipe(process.stdout)
         server.stderr.pipe(process.stderr)
         const browser = await puppeteer.launch()
@@ -103,7 +104,9 @@ module.exports = {
         await page.goto('http://localhost:8000')
         await page.waitFor(1000)
         await page.screenshot({ path: 'static/screenshot.png', fullPage: true })
-        server.kill()
+        const content = await page.content()
+        fs.writeFileSync('static/screenshot-src.html', content)
+        server.kill('SIGINT')
         browser.close()
       }
     ]
@@ -158,7 +161,7 @@ module.exports = {
     async () => {
       const puppeteer = require('puppeteer')
       const fs = require('fs')
-      const server = childProcess.exec('node ./dist/index.js')
+      const server = childProcess.spawn('node', ['./dist/index.js'])
       server.stdout.pipe(process.stdout)
       server.stderr.pipe(process.stderr)
       const browser = await puppeteer.launch()
@@ -171,7 +174,7 @@ module.exports = {
         return element ? element.innerHTML : ''
       })
       fs.writeFileSync('static/prerender.html', content)
-      server.kill()
+      server.kill('SIGINT')
       browser.close()
     },
     'clean-scripts build.front[1]'
