d24e4d9f29d5c55677cb902d65c51151a53815db
york yao <yaoao12306@outlook.com>
2018-01-28 10:33:43 +0800

    chore: update dependencies

--- src/libs.ts
+++ src/libs.ts
@@ -238,10 +238,7 @@ export const specIndexSpecTs = `it("", () => {
 
 export const specKarmaConfigJs = `const webpackConfig = require('./webpack.config.js')
 
-const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision
-const Downloader = require('puppeteer/utils/ChromiumDownloader')
-const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision)
-process.env.CHROME_BIN = revisionInfo.executablePath
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (karma) {
   const config = {
